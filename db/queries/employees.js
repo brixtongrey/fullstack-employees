2034dca9cc2fb09298f895d797b6ca13641f4181
Brixtongrey <brixtonaruda@gmail.com>
2025-10-21 19:16:19 -0600

    feat: updated createEmployee function

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,6 +1,20 @@
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
   // TODO
+ try {
+  const sql = `
+  INSERT INTO employees (name, birthday, salary)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+  `;
+  const values = [name, birthday, salary];
+  const { rows } = await db.query(sql, values);
+  console.log("New employee added");
+  return rows[0];
+ } catch(error) {
+  console.error("Error creating employee", error)
+  throw error;
+ }
 }
 
 // === Part 2 ===
