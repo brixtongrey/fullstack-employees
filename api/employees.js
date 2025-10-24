822924e079e2c0cc6e606ee98925d34b7cd06bfe
Brixtongrey <brixtonaruda@gmail.com>
2025-10-23 19:52:12 -0600

    feat: added GET /employees

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,42 @@
 import express from "express";
 const router = express.Router();
-export default router;
 
-// TODO: this file!
+import { getEmployees, getEmployee, createEmployee, updateEmployee, deleteEmployee } from "#db/queries/employees";
+
+// GET /employees --> return all movies
+router.get("/", async (req, res, next) => {
+    try {
+        const employees = await getEmployees();
+        res.status(200).json(employees);
+    } catch (error) {
+        next(error);
+    }
+});
+
+// GET /employees/:id --> return specific employee
+
+
+
+
+// POST /employees --> create new employee
+
+
+
+
+// PUT /employees/:id --> update an employee
+
+
+
+
+// DELETE /employees/:id --> delete an employee
+
+
+
+
+
+
+
+
+
+
+export default router;
