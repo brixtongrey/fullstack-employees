624a4992687b035e829e908090fd290bf77e71ea
Brixtongrey <brixtonaruda@gmail.com>
2025-10-23 19:43:01 -0600

    feat: added middleware to app.js

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
+import router from "#api/employees";
 import express from "express";
 const app = express();
-export default app;
 
-// TODO: this file!
+// middleware to parse request
+app.use(express.json());
+
+// TODO: route /employees to employee router
+app.use("/employee", router);
+
+// error middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send("Sorry! Something went wrong :(");
+});
+
+export default app;
